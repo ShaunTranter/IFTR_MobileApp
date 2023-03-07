ac61fdb3e0cea7c44f9853eb4feafdb2f66febfd
ShaunTranter <Shaun.Tranter01@gmail.com>
2023-03-07 20:06:08 +0000

    initial Commit

---
+++ app/src/main/java/com/example/iftr/ui/profile/profileFragment.java
@@ -0,0 +1,39 @@
+package com.example.iftr.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.iftr.databinding.FragmentprofileBinding;
+import com.example.iftr.ui.dashboard.DashboardViewModel;
+
+public class profileFragment extends Fragment{
+
+    private FragmentprofileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textDashboard;
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
+
